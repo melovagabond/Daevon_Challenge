330c7f66b3508473ccf790c059817b1d8ac82038
melovagabond <dcbranche246@gmail.com>
2023-07-19 17:34:19 -0400

    Adjustments to userdata.sh and networking.tf

--- website/AWS/ec2/userdata.sh
+++ website/AWS/ec2/userdata.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install -y docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ubuntu
+sudo su
+apt update -y
+apt install -y docker.io git
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ubuntu
+usermod -aG docker root
 
 # Download your project's files to the instance
-sudo apt install -y git
 cd /home/ubuntu || return
 git clone https://github.com/melovagabond/Daevon_Challenge.git
+git pull
 cd Daevon_Challenge/website/docker || return
 mkdir certs
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/server.key -out ./certs/server.crt -subj "/C=US/ST=Pennsylvania/L=Philadelphia/O=Daevonlab/OU=R&D/CN=localhost"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/server.key -out ./certs/server.crt -subj "/C=US/ST=Pennsylvania/L=Philadelphia/O=Daevonlab/OU=R&D/CN=localhost"
+chmod -R 777 ./certs/*
 
 
 # Build and run the Docker container
 sudo docker build -t webpage .
-sudo docker run -d -p 8080:80 --name webpage-container webpage
+sudo docker run -d -p 443:443 --name webpage-container webpage
